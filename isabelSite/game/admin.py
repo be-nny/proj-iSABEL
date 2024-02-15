6f68c2433d83379b1b586230ace5b0db9fd4aa6b
benabbott <ben.abbott1717@gmail.com>
2024-02-15 15:24:40 +0000

    started user model

--- isabelSite/game/admin.py
+++ isabelSite/game/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import MyUser
+
+class MyAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = MyUser
+    list_display = ["email", "username",]
+
+admin.site.register(MyUser, MyAdmin)
