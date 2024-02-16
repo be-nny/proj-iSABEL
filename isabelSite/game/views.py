63f7d639cffa4ec90191bbbd11aea8ee9bddef17
benabbott <ben.abbott1717@gmail.com>
2024-02-16 15:20:03 +0000

    comments

--- isabelSite/game/views.py
+++ isabelSite/game/views.py
@@ -1,51 +1,69 @@
 from django.contrib.auth import logout
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.conf import settings
-from django.shortcuts import redirect
-
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
-
 from .forms import CustomUserCreationForm
 
-# Create your views here.
+"""
+Initialises the login and sign up flow
+"""
 class SignUp(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
 
+"""
+View for the scan page, if a user isn't logged in, they are redirected
+"""
 def scan(request):
     if not request.user.is_authenticated:
         return userNotLoggedIn(request)
     else:
         return render(request, "site/scan.html", {})
-
+"""
+When a user isn't logged in, they are redirected to the log in and sign up page
+"""
 def userNotLoggedIn(request):
     logout(request)
     return render(request, "registration/login-signup.html", {})
 
+"""
+View for the login and sign up page
+"""
 def loginSignup(request):
     return render(request, "registration/login-signup.html", {})
 
+"""
+View for the user map page, if a user isn't logged in, they are redirected
+"""
 def userMap(request):
     if not request.user.is_authenticated:
         return userNotLoggedIn(request)
     else:
         return HttpResponse("Map page")
 
+"""
+View for the leaderboard page, if a user isn't logged in, they are redirected
+"""
 def leaderboard(request):
     if not request.user.is_authenticated:
         return userNotLoggedIn(request)
     else:
         return HttpResponse("Leaderboard page")
 
+"""
+View for the users profile page, if a user isn't logged in, they are redirected
+"""
 def profile(request):
     if not request.user.is_authenticated:
         return userNotLoggedIn(request)
     else:
         return HttpResponse("Profile page")
 
+"""
+View for the about page, if a user isn't logged in, they are redirected
+"""
 def about(request):
     if not request.user.is_authenticated:
         return userNotLoggedIn(request)
