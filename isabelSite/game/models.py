d47b1078c2f2a296c62330a560ca4a3af2066a75
ipekbal <mrvipekbal@gmail.com>
2024-02-18 19:13:27 +0000

    created user manager, added get methods to the user class

--- isabelSite/game/models.py
+++ isabelSite/game/models.py
@@ -1,7 +1,27 @@
 import uuid
 
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
+
+
+class CustomUserManager(UserManager):
+    def _create_user(self, email,password, **extra_fields):
+        if not email:
+            raise ValueError("You have not provided a valid e-mail address")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    def create_user(self, email=None,password=None, **extra_fields):
+        extra_fields.setdefault('is_game_keeper', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email,password,**extra_fields)
+
+    def create_superuser(self, email=None,password=None, **extra_fields):
+        extra_fields.setdefault('is_game_keeper', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(email,password,**extra_fields)
 
 
 # Create your models here.
@@ -24,6 +44,35 @@ class MyUser(AbstractUser):
     leaderboard_position = models.IntegerField()
     longest_streak = models.IntegerField(default=0)
     golden_bins_collected = models.IntegerField(default=0)
+    is_game_keeper = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def get_username(self):
+        return self.username
+    def get_first_name(self):
+        return self.first_name
+    def get_short_name(self):
+        return self.first_name or self.email.split('@')[0]
+    def get_user_xp(self):
+        return self.user_xp
+    def get_weight_recycled(self):
+        return self.weight_recycled
+    def get_streak(self):
+        return self.streak
+    def get_profile_pic(self):
+        return self.profile_pic
+    def get_total_weight_recycled(self):
+        return self.total_weight_recycled
+    def get_leaderboard_position(self):
+        return self.leaderboard_position
+    def get_longest_streak(self):
+        return self.longest_streak
+    def get_golden_bins_collected(self):
+        return self.golden_bins_collected
 
     def __str__(self):
         return self.username
