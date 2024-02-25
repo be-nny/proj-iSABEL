3843f39b95e3dc36bfc73675b3651a61a1981e90
osticode <samjpulsford@gmail.com>
2024-02-25 17:22:17 +0000

    Created form in expDemo.html to capture example details and updating the user exp.

--- isabelSite/game/templatetags/exp_tags.py
+++ isabelSite/game/templatetags/exp_tags.py
@@ -5,12 +5,16 @@ def calculate_points_to_add(param1, param2, param3):
     param1_weight = 1
     param2_weight = 1
     param3_weight = 1
-    points = (param1_weight*param1 + param2_weight*param2 + param3_weight*param3)
+    points = (param1_weight*int(param1) + param2_weight*int(param2) + param3_weight*int(param3))
     return points
 
 @register.simple_tag
-def update_user_points(user):
-    points_assigned = calculate_points_to_add(1, 1, 1)
+def update_user_exp(user, isvegan, isplastic, isheavy):
+    points_assigned = calculate_points_to_add(isvegan, isplastic, isheavy)
     user.user_xp += points_assigned
     user.save()
     return points_assigned
+
+@register.simple_tag
+def update_user_exp_from_form(user, request):
+    return update_user_exp(user, request.GET.get("isvegan", "0"), request.GET.get("isplastic", "0"), request.GET.get("isheavy", "0"))
