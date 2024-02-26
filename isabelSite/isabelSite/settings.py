40080945aace8efe4de6a0635948af3452df04d0
benabbott <ben.abbott1717@gmail.com>
2024-02-26 11:29:11 +0000

    removed crispy forms

--- isabelSite/isabelSite/settings.py
+++ isabelSite/isabelSite/settings.py
@@ -37,12 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'django_user_agents',
-    'crispy_bootstrap4',
-    'crispy_forms',
     "game",
 ]
-
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
