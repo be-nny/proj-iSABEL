cb87598cc50ef711eac528c9a1d4d11c16211665
benabbott <ben.abbott1717@gmail.com>
2024-02-28 18:29:50 +0000

    Author: Ellis mackness (laptop broke) started work on linking barcode to point system

--- isabelSite/static/scripts/camera.js
+++ isabelSite/static/scripts/camera.js
@@ -1,8 +1,13 @@
 const html5QrCode = new Html5Qrcode("reader");
 const qrCodeSuccessCallback = (decodedText, decodedResult) => {
     console.log(decodedText)
+    console.log(decodedResult)
+    let code_Result = decodedResult.result.format.formatName
+    if (code_Result != 'QR_CODE'){
+        //TODO run python point calculator
+    }
 };
-const config = { fps: 10, qrbox: { width: 250, height: 250 } };
+const config = { fps: 10, qrbox: { width: 400, height: 400 } };
 
 // If you want to prefer back camera
 html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
