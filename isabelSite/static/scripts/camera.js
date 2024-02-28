72c37a4aee1e9fa752bcb0ee2dcfbfea8c3a3799
benabbott <ben.abbott1717@gmail.com>
2024-02-28 17:52:44 +0000

    Author: Ben Abbott got the easy mode working

--- isabelSite/static/scripts/camera.js
+++ isabelSite/static/scripts/camera.js
@@ -1,49 +1,17 @@
+// This method will trigger user permission
+function onScanSuccess(decodedText, decodedResult) {
+  // handle the scanned code as you like, for example:
+  console.log(`Code matched = ${decodedText}`, decodedResult);
+}
 
+function onScanFailure(error) {
+  // handle scan failure, usually better to ignore and keep scanning.
+  // for example:
+  console.warn(`Code scan error = ${error}`);
+}
 
-// This method will trigger user permissions
-Html5Qrcode.getCameras().then(devices => {
-  /**
-   * devices would be an array of objects of type:
-   * { id: "id", label: "label" }
-   */
-  if (devices && devices.length) {
-    var cameraId = devices[0].id;
-    console.log(cameraId);
-  }
-}).catch(err => {
-  // handle err
-});
-
-const html5QrCode = new Html5Qrcode(/* element id */ "reader");
-html5QrCode.start(
-  cameraId,
-  {
-    fps: 10,    // Optional, frame per seconds for qr code scanning
-    qrbox: { width: 250, height: 250 }  // Optional, if you want bounded box UI
-  },
-  (decodedText, decodedResult) => {
-    // do something when code is read
-  },
-  (errorMessage) => {
-    // parse error, ignore it.
-  })
-.catch((err) => {
-  // Start failed, handle it.
-});
-
-const html5QrCode = new Html5Qrcode("reader", /* verbose= */ true);
-
-const html5QrCode = new Html5Qrcode("reader");
-const qrCodeSuccessCallback = (decodedText, decodedResult) => {
-    /* handle success */
-};
-const config = { fps: 10, qrbox: { width: 250, height: 250 } };
-
-// If you want to prefer back camera
-html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
-
-html5QrCode.stop().then((ignore) => {
-  // QR Code scanning is stopped.
-}).catch((err) => {
-  // Stop failed, handle it.
-});
+let html5QrcodeScanner = new Html5QrcodeScanner(
+  "reader",
+  { fps: 10, qrbox: {width: 250, height: 250} },
+  /* verbose= */ false);
+html5QrcodeScanner.render(onScanSuccess, onScanFailure);
