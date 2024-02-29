44dda29bb68732c0771396bb73b9e9396783e560
benabbott <ben.abbott1717@gmail.com>
2024-02-29 15:00:39 +0000

    Author: Ben Abbot Cool css border around video frame

--- isabelSite/game/templatetags/password_reset_tags.py
+++ isabelSite/game/templatetags/password_reset_tags.py
@@ -1,28 +1,28 @@
-import smtplib
 from django import template
 from django.core.mail import send_mail
+from django.conf import settings
+
 import random
-
-
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
 register = template.Library()
 
 @register.simple_tag
 def sendResetCode(request):
-    email = request.GET.get('email', '')
-
+    usr_email = request.GET.get('email', '')
     code = randomCode()
-    if email != '':
-        try:
-            send_mail("iSABEL Password Reset Code",
-                  "Here is your password reset code: " + code,
-                  "from@example.com",
-                  [email],
-                  fail_silently=False,)
-        except smtplib.SMTPException:
-            print("could not send email.")
+
+    if usr_email != '':
+        message = "Here is your password reset code " + code
+
+        subject = "Password Reset"
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = [usr_email, ]
+        send_mail(subject, message, email_from, recipient_list)
+
         return True
-    else:
-        return False
+    return False
+
 def randomCode():
     return ''.join(str(random.randint(0, 9)) for _ in range(6))
--- isabelSite/isabelSite/settings.py
+++ isabelSite/isabelSite/settings.py
@@ -136,3 +136,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_REDIRECT_URL = "scan"
 LOGOUT_REDIRECT_URL = "logout_view"
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "user.info.isabel@gmail.com"
+EMAIL_HOST_PASSWORD = "99bQ$%5yvO1B"
