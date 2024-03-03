52f5a1ef666334d18aff62240dc0a8a4a38120dc
benabbott <ben.abbott1717@gmail.com>
2024-03-03 17:11:19 +0000

    linked front and back end

--- isabelSite/static/scripts/camera.js
+++ isabelSite/static/scripts/camera.js
@@ -1,13 +1,40 @@
 const html5QrCode = new Html5Qrcode("reader");
+let prev_code = "";
 const qrCodeSuccessCallback = (decodedText, decodedResult) => {
-    console.log(decodedText)
-    console.log(decodedResult)
     let code_Result = decodedResult.result.format.formatName
     if (code_Result != 'QR_CODE'){
-        //TODO run python point calculator
+        if(prev_code != decodedText) {
+            prev_code = decodedText
+            if (decodedText == "") {
+                updateUserFromBCode("0");
+            } else {
+                updateUserFromBCode(decodedText);
+            }
+        }
+    } else{
+        //TODO QR BINS
     }
 };
-const config = { fps: 10, qrbox: { width: 400, height: 400 } };
 
+const config = { fps: 10, qrbox: { width: 400, height: 400 } };
 // If you want to prefer back camera
 html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
+
+function updateUserFromBCode(decodedText) {
+    // Send AJAX request to Django view
+    fetch(`update_user_from_bcode?code=${decodedText}`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Update successful:', data);
+        // Handle success if needed
+    })
+    .catch(error => {
+        console.error('Error updating user:', error);
+        // Handle error if needed
+    });
+}
