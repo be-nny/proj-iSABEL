95f8cb72b119ba2515bc63a03180c0c459003ac4
ipekbal <mrvipekbal@gmail.com>
2024-03-17 18:52:55 +0000

    report created when a message is submitted

--- isabelSite/static/scripts/report.js
+++ isabelSite/static/scripts/report.js
@@ -1,9 +1,35 @@
 document.addEventListener('DOMContentLoaded', function () {
         var reportButton = document.getElementById('reportButton');
-        var reportForm = document.getElementById('reportForm');
+        var reportForm = document.getElementById('reportFormDiv');
 
         // Toggle the display of the form when the button is clicked
         reportButton.addEventListener('click', function () {
             reportForm.classList.toggle('hide');
+        });
+    })
+
+document.getElementById('reportForm').addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent default form submission
+    var form = this;
+    var formData = new FormData(form);
+
+    fetch(form.action, {
+        method: 'POST',
+        body: formData,
+        headers: {
+            'X-CSRFToken': '{{ csrf_token }}'
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                alert('Report saved successfully!');
+                form.reset(); // Clear form fields
+            } else {
+                alert('Error: ' + data.error);
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
         });
     });
