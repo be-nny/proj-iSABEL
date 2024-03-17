79c960d02375068adcc33a874bbbc1f86278a5a2
benabbott <ben.abbott1717@gmail.com>
2024-03-17 17:48:38 +0000

    BEN ABBOTT: changed email backend

--- isabelSite/isabelSite/settings.py
+++ isabelSite/isabelSite/settings.py
@@ -150,4 +150,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "user.info.isabel@gmail.com"
-EMAIL_HOST_PASSWORD = "dhvf rufo ujqk ifny"
+EMAIL_HOST_PASSWORD = "oeeqzngnnuoufkiq"
