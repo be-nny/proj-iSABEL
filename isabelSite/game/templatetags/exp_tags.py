5e1913b711d5467cc86da0029a074a1acf1af050
benabbott <ben.abbott1717@gmail.com>
2024-03-18 19:02:28 +0000

    altered chrome options

--- isabelSite/game/templatetags/exp_tags.py
+++ isabelSite/game/templatetags/exp_tags.py
@@ -47,10 +47,7 @@ def getAttributes(code):
     meats = []
 
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--disable-dev-shm-usage")
     dr = webdriver.Chrome(options=chrome_options)
 
     dr.get(url)
