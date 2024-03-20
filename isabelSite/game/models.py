4874f8b93877bde17bb62ccbaec7f8a567746fa8
benabbott <ben.abbott1717@gmail.com>
2024-03-20 16:20:15 +0000

    AUTHOR BEN ABBOTT: rolled back permission changes

--- isabelSite/game/models.py
+++ isabelSite/game/models.py
@@ -33,8 +33,6 @@ class MyUser(AbstractUser):
     leaderboard_position = models.IntegerField(default=-1)
     longest_streak = models.IntegerField(default=0)
     golden_bins_collected = models.IntegerField(default=0)
-    is_game_keeper = models.BooleanField(default=False)
-    # is_superuser = models.BooleanField(default=False)
 
     # Define methods to retrieve user information
     def get_username(self):
