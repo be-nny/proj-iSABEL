0933e2886764322832044434ae34f92b9ac7a725
benabbott <ben.abbott1717@gmail.com>
2024-03-20 13:54:28 +0000

    AUTHOR BEN ABBOTT: report ui

--- isabelSite/game/views.py
+++ isabelSite/game/views.py
@@ -180,5 +180,9 @@ def reports(request):
     if not request.user.is_authenticated:
         return userNotLoggedIn(request)
     else:
-        return render(request, 'gamekeeper/reports.html', {})
-
+        mydata = Report.objects.all()
+        template = loader.get_template('gamekeeper/reports.html')
+        context = {
+            'reports': mydata,
+        }
+        return HttpResponse(template.render(context, request))
